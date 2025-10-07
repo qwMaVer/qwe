9cdb2f4d6260af87995b66f81a14ccb648f7b502
qwMaVer <gatilinmatvej@gmail.com>
2025-10-07 20:23:01 +0400

    21212

---
+++ src/AndroidCompact.jsx
@@ -0,0 +1,390 @@
+import "./style.css";
+import React from "react";
+import cb5cc9941359e0bde3afe69b8ceda42XDesign1Photoroom1 from "./1cb5cc9941359e0bde3afe69b8ceda42-x-design-1-photoroom-1.png";
+import cc5ebdf324104350a26d7cc22220f2fXDesign1Photoroom2 from "./1cc5ebdf324104350a26d7cc22220f2f-x-design-1-photoroom-2.png";
+import fb95be91b9565c4c90ad607b08d797XDesignPhotoroom1 from "./12fb95be91b9565c4c90ad607b08d797-x-design-photoroom-1.png";
+import b1f8e41efffefe97bda2621faddXDesignPhotoroom1 from "./97251b1f8e41efffefe97bda2621fadd-x-design-photoroom-1.png";
+import f4a48da2df40c6cff970e6fa46XDesignPhotoroom1 from "./107208f4a48da2df40c6cff970e6fa46-x-design-photoroom-1.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom2 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-2.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom3 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-3.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom4 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-4.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom5 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-5.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom6 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-6.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom7 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-7.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom8 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-8.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom9 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-9.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom10 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-10.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom11 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-11.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom12 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-12.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom13 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-13.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom14 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-14.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom15 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-15.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom16 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-16.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom17 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-17.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom18 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-18.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom19 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-19.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom20 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-20.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom21 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-21.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom22 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-22.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom23 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-23.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom24 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-24.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom25 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-25.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom26 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-26.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom27 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-27.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom28 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-28.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom29 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-29.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom30 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-30.png";
+import a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom31 from "./a5dfbc426fa9bfbaa937a11b896d6c9a-photoroom-31.png";
+import airbrushImageEnhancer1759422993219Photoroom1 from "./airbrush-image-enhancer-1759422993219-photoroom-1.png";
+import airbrushImageEnhancer1759423142507Photoroom1 from "./airbrush-image-enhancer-1759423142507-photoroom-1.png";
+import airbrushImageEnhancer1759425871700Photoroom1 from "./airbrush-image-enhancer-1759425871700-photoroom-1.png";
+import black1 from "./black-1.png";
+import dfabe73df0b5dcf9d5bf07e1a5c430cbPhotoroom1 from "./dfabe73df0b5dcf9d5bf07e1a5c430cb-photoroom-1.png";
+import ellipse1 from "./ellipse-1.png";
+import fc503b86e91981e1ed6a7c449eee2b58XDesignPhotoroom1 from "./fc503b86e91981e1ed6a7c449eee2b58-x-design-photoroom-1.png";
+import fc503b86e91981e1ed6a7c449eee2b58XDesignPhotoroom2 from "./fc503b86e91981e1ed6a7c449eee2b58-x-design-photoroom-2.png";
+import matveyG from "./matvey-g.png";
+import phocaThumbLUzor226Photoroom1 from "./phoca-thumb-l-uzor-226-photoroom-1.png";
+import rectangle10 from "./rectangle-10.png";
+import "./style.css";
+import vector1 from "./vector-1.svg";
+import vector2 from "./vector-2.svg";
+import vector3 from "./vector-3.svg";
+import vector4 from "./vector-4.svg";
+import vector5 from "./vector-5.svg";
+import vector6 from "./vector-6.svg";
+import vector7 from "./vector-7.png";
+import vector8 from "./vector-8.svg";
+import vector9 from "./vector-9.svg";
+
+export const AndroidCompact = () => {
+  return (
+    <div className="android-compact">
+      <img className="black" alt="Black" src={black1} />
+
+      <img
+        className="img"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom2}
+      />
+
+      <img
+        className="img-2"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom3}
+      />
+
+      <img
+        className="img-3"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom4}
+      />
+
+      <img
+        className="img-4"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom5}
+      />
+
+      <img
+        className="img-5"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom6}
+      />
+
+      <img
+        className="img-6"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom7}
+      />
+
+      <img
+        className="img-7"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom31}
+      />
+
+      <img
+        className="img-8"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom8}
+      />
+
+      <img
+        className="img-9"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom9}
+      />
+
+      <img
+        className="img-10"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom10}
+      />
+
+      <img
+        className="img-11"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom11}
+      />
+
+      <img
+        className="img-12"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom12}
+      />
+
+      <img
+        className="img-13"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom13}
+      />
+
+      <img
+        className="img-14"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom26}
+      />
+
+      <img
+        className="img-15"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom29}
+      />
+
+      <img
+        className="img-16"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom30}
+      />
+
+      <img
+        className="img-17"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom14}
+      />
+
+      <img
+        className="img-18"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom15}
+      />
+
+      <img
+        className="img-19"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom16}
+      />
+
+      <img
+        className="img-20"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom17}
+      />
+
+      <img
+        className="img-21"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom18}
+      />
+
+      <img
+        className="img-22"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom19}
+      />
+
+      <img
+        className="img-23"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom20}
+      />
+
+      <img
+        className="img-24"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom27}
+      />
+
+      <img
+        className="img-25"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom21}
+      />
+
+      <img
+        className="img-26"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom22}
+      />
+
+      <img
+        className="img-27"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom28}
+      />
+
+      <img
+        className="img-28"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom24}
+      />
+
+      <img
+        className="img-29"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom25}
+      />
+
+      <img
+        className="img-30"
+        alt="Img"
+        src={a5dfbc426fa9bfbaa937a11b896d6c9aPhotoroom23}
+      />
+
+      <img
+        className="phoca-thumb-l-uzor"
+        alt="Phoca thumb l uzor"
+        src={phocaThumbLUzor226Photoroom1}
+      />
+
+      <img className="vector" alt="Vector" src={vector3} />
+
+      <img className="vector" alt="Vector" src={vector4} />
+
+      <img className="vector" alt="Vector" src={vector5} />
+
+      <img className="vector" alt="Vector" src={vector6} />
+
+      <div className="rectangle" />
+
+      <img className="rectangle-2" alt="Rectangle" src={rectangle10} />
+
+      <div className="div" />
+
+      <img className="vector-2" alt="Vector" src={vector1} />
+
+      <img className="vector-3" alt="Vector" src={vector2} />
+
+      <div className="rectangle-3" />
+
+      <div className="rectangle-4" />
+
+      <div className="rectangle-5" />
+
+      <div className="rectangle-6" />
+
+      <div className="rectangle-7" />
+
+      <div className="rectangle-8" />
+
+      <img
+        className="airbrush-image"
+        alt="Airbrush image"
+        src={airbrushImageEnhancer1759422993219Photoroom1}
+      />
+
+      <img
+        className="airbrush-image-2"
+        alt="Airbrush image"
+        src={airbrushImageEnhancer1759423142507Photoroom1}
+      />
+
+      <img
+        className="img-31"
+        alt="Img"
+        src={dfabe73df0b5dcf9d5bf07e1a5c430cbPhotoroom1}
+      />
+
+      <img className="ellipse" alt="Ellipse" src={ellipse1} />
+
+      <div className="text">{""}</div>
+
+      <img className="matvey-g" alt="Matvey g" src={matveyG} />
+
+      <img
+        className="img-32"
+        alt="Img"
+        src={fb95be91b9565c4c90ad607b08d797XDesignPhotoroom1}
+      />
+
+      <img
+        className="airbrush-image-3"
+        alt="Airbrush image"
+        src={airbrushImageEnhancer1759425871700Photoroom1}
+      />
+
+      <img
+        className="img-33"
+        alt="Img"
+        src={fc503b86e91981e1ed6a7c449eee2b58XDesignPhotoroom1}
+      />
+
+      <img
+        className="img-34"
+        alt="Img"
+        src={fc503b86e91981e1ed6a7c449eee2b58XDesignPhotoroom2}
+      />
+
+      <div className="text-wrapper">ИНВЕНТАРЬ</div>
+
+      <img
+        className="img-35"
+        alt="Img"
+        src={f4a48da2df40c6cff970e6fa46XDesignPhotoroom1}
+      />
+
+      <img
+        className="img-36"
+        alt="Img"
+        src={cc5ebdf324104350a26d7cc22220f2fXDesign1Photoroom2}
+      />
+
+      <img
+        className="img-37"
+        alt="Img"
+        src={b1f8e41efffefe97bda2621faddXDesignPhotoroom1}
+      />
+
+      <img
+        className="img-38"
+        alt="Img"
+        src={cb5cc9941359e0bde3afe69b8ceda42XDesign1Photoroom1}
+      />
+
+      <div className="text-wrapper-2">ПОПОЛНИТЬ</div>
+
+      <div className="text-wrapper-3">скидка 30%</div>
+
+      <div className="text-wrapper-4">САПЕР</div>
+
+      <div className="text-wrapper-5">не попадись на мины</div>
+
+      <div className="text-wrapper-6">КРУТИТЬ</div>
+
+      <div className="text-wrapper-7">делай иксы</div>
+
+      <img className="vector-4" alt="Vector" src={vector7} />
+
+      <img className="vector-5" alt="Vector" src={vector9} />
+
+      <img className="vector-6" alt="Vector" src={vector8} />
+
+      <div className="text-wrapper-8">x10</div>
+
+      <div className="text-wrapper-9">шанс</div>
+
+      <p className="p">
+        <span className="span">крути и </span>
+
+        <span className="text-wrapper-10">выигрывай</span>
+      </p>
+
+      <div className="rectangle-9" />
+
+      <div className="text-wrapper-11">Здесь будет менюшка</div>
+
+      <div className="text-wrapper-12">7779</div>
+    </div>
+  );
+};
